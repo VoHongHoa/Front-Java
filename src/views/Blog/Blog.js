2c706f82b7c7ba6b7d45b44fffd87bbf914ad412
ducthien <19522261@gm.uit.edu.vn>
2022-06-08 13:28:39 +0700

    thien

--- src/views/Blog/Blog.js
+++ src/views/Blog/Blog.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./Blog.scss";
 import HomeHeader from "../Homepage/HomeHeader";
 import Footer from "../Homepage/Footer";
+import { NavLink } from "react-router-dom";
+import { withRouter } from "react-router";
 
 class Blog extends Component {
   render() {
@@ -10,6 +12,28 @@ class Blog extends Component {
         <div className="mb-2">
           <HomeHeader></HomeHeader>
         </div>
+        <section id="sidebar">
+          <p>
+            <NavLink to="/" style={{ color: "black", cursor: "pointer" }}>
+              Trang chủ
+            </NavLink>{" "}
+            | <b>Blog</b>
+          </p>
+          <h1 className="title-blog">Cập nhật tin tức</h1>
+          <h5 className="h5">Tin tức:</h5>
+          <NavLink exact to="./blog/:idblog">
+            <div class="blog-card">
+              <div class="card-body">
+                <p class="card-text">
+                  Biến thể dùng chip Dimensity 9000 của Xiaomi 12S Pro vừa đạt
+                  chứng nhận 3C, sắp sửa ra mắt?
+                </p>
+                <br />
+                <p className="date">Ngày 1/6/2022</p>
+              </div>
+            </div>
+          </NavLink>
+        </section>
         <div className="content-blog"> Blog here</div>
         <div className="mt-2">
           <Footer />
@@ -18,4 +42,4 @@ class Blog extends Component {
     );
   }
 }
-export default Blog;
+export default withRouter(Blog);
